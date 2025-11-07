68746254095a58319197c738d689f3d48f259ad5
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-11-07 15:06:31 +0100

    fix: Removed clickable links in favour of opening them directly (Closes #19)

---
+++ src/lib/open-url.ts
@@ -0,0 +1,29 @@
+import { exec } from "node:child_process"
+
+export const openUrl = (url: string, message: string = ""): void => {
+	let command: string
+
+	url = url.replaceAll("(", "%28").replaceAll(")", "%29")
+
+	switch (process.platform) {
+		case "darwin": // macOS
+			command = `open "${url}"`
+			break
+		case "win32": // Windows
+			command = `start "${url.replaceAll("&", "^&")}"`
+			break
+		default: // Linux and other POSIX-like systems
+			command = `xdg-open "${url}"`
+			break
+	}
+
+	exec(command, (error) => {
+		if (error) {
+			console.error(`Error opening URL: ${error.message}`)
+			return
+		}
+
+		const messageStr = message.length > 0 ? `${message}: ` : ""
+		console.log(`${messageStr}${url}`)
+	})
+}
